f652d757e59392254251c630d2de0890e27522e7
Alex Kindle <alex@alexkindle.com>
2016-07-24 13:07:05 -0400

    release 1.0

--- clip2imgur/Properties/AssemblyInfo.cs
+++ clip2imgur/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CracklePop")]
+[assembly: AssemblyTitle("clip2imgur")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CracklePop")]
+[assembly: AssemblyProduct("clip2imgur")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
