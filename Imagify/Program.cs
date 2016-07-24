ed23cd8426436b78b433afb9454a44e6a6e7b4dc
Alex Kindle <alex@alexkindle.com>
2016-07-24 12:58:09 -0400

    initial commit. pretty much works and does everything i want!

---
+++ Imagify/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using ClipboardMonitor;
+using Imgur.API;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
+
+namespace Imagify
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            ClipboardNotifications.ClipboardUpdate += ClipboardChanged;
+            while (true) Thread.Sleep(1000);
+        }
+
+        private static async void ClipboardChanged(object sender, EventArgs eventArgs)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                Console.WriteLine("Clipboard image event occurred, capturing and rewriting...");
+
+                try
+                {
+                    var client = new ImgurClient(Secrets.ClientId, Secrets.ClientSecret);
+                    var ep = new ImageEndpoint(client);
+                    using (var mem = new MemoryStream())
+                    {
+                        using (var i = Clipboard.GetImage())
+                        {
+                            i.Save(mem, ImageFormat.Png);
+                        }
+                        // var res = ep.UploadImageStreamAsync(mem);
+                        // var img = res.Result;
+                        var img = await ep.UploadImageBinaryAsync(mem.GetBuffer());
+                        Console.WriteLine(img.Link);
+                        Clipboard.SetText(img.Link);
+                    }
+                }
+                catch (ImgurException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
